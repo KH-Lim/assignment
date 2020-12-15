ec58a617f097dd3f4d0d11e713e7d3b2f59ddb81
KihoonLim <khim@playauto.co.kr>
2020-12-15 16:44:38 +0900

    commit

--- src/List.tsx
+++ src/List.tsx
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from "react";
+import { DataGrid } from '@material-ui/data-grid';
+
+
 
 export default function List() {
   // fixme: error를 수정해 주세요
   // https://material-ui.com/components/tables/ 을 사용해서 comments를 테이블에 보여주세요
+  const [list, setList] = useState([]);
 
-  const [list, setList] = useState();
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/comments")
       .then((response) => response.json())
@@ -14,5 +17,36 @@ export default function List() {
       });
   }, []);
 
-  return <>{JSON.stringify(list)}</>;
+  // 테이블 컬럼 정보
+  const columns = [
+    {
+      field: 'id',
+      headerName: 'ID',
+    },
+    {
+      field: 'email',
+      headerName: 'Email',
+    },
+    {
+      field: 'name',
+      headerName: 'Name',
+    },
+    {
+      field: 'postId',
+      headerName: 'postId',
+    },
+    {
+      field: 'body',
+      headerName: 'body',
+    },
+  ]
+
+
+  // return <>{JSON.stringify(list)}</>;
+  return <>
+    <div style={{ height: 700, width: '100%' }}>
+      <DataGrid rows={list} columns={columns} pageSize={10} checkboxSelection />
+    </div>
+  </>;
+
 }
